f17278d93ab986763b0c4049d7ba7cd2eef85ffe
eric kisendi <erickisendi18@gmail.com>
2023-06-14 13:37:14 +0300

    api intro

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const express = require('express');
-const path = require('path');
+//const path = require('path');
 const app = express();
+const {products} = require('./data.js')
 
-//set up static and middleware
-app.use(express.static('./public'))
+app.get('/', (req, res) =>{
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+})
 
-// app.get('/', (req, res) =>{
-//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-// })
+app.get('/api/products', (req, res) =>{
+    const newProduct = products.map((product) =>{
+        const {id, name, image} = product;
+        return {id, name, image}
+    })
 
-app.all('*', (req, res) =>{
-    res.status(404).send('Resource not found')
+    res.json(newProduct)
+})
+
+app.get('/api/products/:productID', (req, res) =>{
+     
+    const {productID} = req.params;
+    const singleProduct = products.find((product) => product.id === Number(productID))
+
+    res.json(singleProduct)
 })
 
 app.listen(5000, () =>{
-    console.log('listening on port 5000....');
+    console.log('listening on port 5000 ...');
 })
-//app.get 
-//app.post
-//app.put
-//app.delete
-//app.all
-//app.use
-//app.listen
