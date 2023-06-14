7df4401ef9ccc2a39e157da8bc4719fb492b3206
eric kisendi <erickisendi18@gmail.com>
2023-06-14 16:23:15 +0300

    update

--- app.js
+++ app.js
@@ -12,16 +12,44 @@ app.get('/api/products', (req, res) =>{
         const {id, name, image} = product;
         return {id, name, image}
     })
-
     res.json(newProduct)
 })
 
 app.get('/api/products/:productID', (req, res) =>{
      
     const {productID} = req.params;
-    const singleProduct = products.find((product) => product.id === Number(productID))
-
+    const singleProduct = products.find(
+        (product) => product.id === Number(productID)
+    )
+    if (!singleProduct){
+        return res.status(404).send('Product does not exist')
+    }
     res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewId', (req, res) => {
+    console.log(req.params);
+    res.send('hello world')
+})
+
+app.get('/api/v1/query', (req, res) => {
+    //console.log(req.query);
+    const {search , limit} = req.query;
+    let sortedProducts = [...products];
+
+    if(search){
+        sortedProducts = sortedProducts.filter((product) =>{
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if (sortedProducts < 1){
+        //res.status(200).send('no products found starting with b')
+        return res.status(200).json({success:true, data:[]})
+    }
+    res.status(200).json(sortedProducts)
 })
 
 app.listen(5000, () =>{
